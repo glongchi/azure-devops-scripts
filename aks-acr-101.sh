ea06d58e72f425e643feb4457bea3864d2f9f454
glongchi <glongchi@gmail.com>
2021-03-07 23:44:52 -0500

    aks-acr-101 checkin

---
+++ aks-acr-101.sh
@@ -0,0 +1,42 @@
+#!/bin/bash
+# aks-acr-101.sh
+  
+az account show -o table
+az account list -o table
+az account set -s 'Pay-As-You-Go'
+az subscription -n 'Pay-As-You-Go'
+
+
+# AKS
+rgName='kzaksacr-rg' #'aks-solution'
+aksName='kzaksacr-101'
+location='eastus'
+
+
+# create AKS cluster
+####################
+acrName='kzacr'
+az group create -l $location -n $rgName #--subscription $appSubId
+
+# Azure Container Registry
+az acr create --resource-group $rgName  --name $acrName --sku Basic
+
+acrResourceId=$(az acr show --name $acrName --resource-group $rgName --query "id" --output tsv)
+az acr update -n $acrName --admin-enabled true
+acr_userName=$(az acr credential show -n $acrName --query="username" -o tsv)
+acr_pwd=$(az acr credential show -n $acrName --query="passwords[0].value" -o tsv)
+echo $acr_userName $acr_pwd
+
+
+az aks create --resource-group $rgName --name $aksName \
+--node-count 1 \
+ --enable-addons monitoring \
+ --generate-ssh-keys \
+ --attach-acr $acrResourceId \
+  #--enable-addons monitoring --workspace-resource-id $logWorkspaceResourceId 
+
+az aks get-credentials -n $aksName -g $rgName
+ 
+az aks show  -n $aksName -g $rgName
+
+kubectl create -f https://dev.azure.com/koomzo/azure%20consulting/_apis/sourceProviders/TfsGit/filecontents?repository=aksacr101&commitOrBranch=master&api-version=5.0-preview.1&path=%2Faksacr101%2Fdeployaks101.yaml
